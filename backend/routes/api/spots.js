46cb443ba039f6c45d54f9a4b24af1de5963ecf8
Lainey <misslenlen@gmail.com>
2024-10-18 16:57:16 -0700

    updated backend to include images in / create a spot route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -299,6 +299,7 @@ router.put("/:spotId", requireAuth, validateSpot, async (req, res) => {
 });
 
 //* Create a Spot (v2) (CHECKED)
+//* Create a Spot
 router.post(
   "/",
   validateSpot, // Use validateSpot middleware to handle validation
@@ -318,6 +319,7 @@ router.post(
         name,
         description,
         price,
+        imageUrls, // FIXED need to ensure server can handle this in form submission
       } = req.body;
 
       // Create the new spot in the database
@@ -334,6 +336,16 @@ router.post(
         price,
       });
 
+      // FIXED CREATE spot images in database
+      if (imageUrls && imageUrls.length) {
+        const spotImages = imageUrls.map((url, index) => ({
+          spotId: newSpot.id,
+          url,
+          preview: index === 0, // Set first image as the preview image
+        }));
+
+        await SpotImage.bulkCreate({ spotImages });
+      }
       // Return the newly created spot
       return res.status(201).json({
         id: newSpot.id,
@@ -351,6 +363,7 @@ router.post(
         updatedAt: newSpot.updatedAt,
       });
     } catch (err) {
+      console.error(err); // DEBUGGING
       return res.status(401).json({ message: "Unauthorized" });
     }
   }
